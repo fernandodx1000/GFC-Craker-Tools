852dbf7327579f537c42006fa3da16b176d758d1
GFC <notyourBusness@fuck.off>
2018-07-08 00:31:47 +0100

    more

--- GFC Craker Tools/Properties/AssemblyInfo.cs
+++ GFC Craker Tools/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.161.188")]
-[assembly: AssemblyFileVersion("1.0.161.188")]
+[assembly: AssemblyVersion("1.0.164.188")]
+[assembly: AssemblyFileVersion("1.0.164.188")]
 
-[assembly: AssemblyInformationalVersion("1.0.159")]
+[assembly: AssemblyInformationalVersion("1.0.162")]
